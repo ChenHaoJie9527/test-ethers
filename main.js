a60a7a2d6fe42d0824c6528d3eb5b7915979c98a
ChenHaoJie9527 <a18520145493@163.com>
2024-02-10 23:59:20 +0800

    feat: linkInfuraNode

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 import { ethers } from "ethers";
 
-const provider = new ethers.BrowserProvider(window.ethereum)
+const provider = new ethers.BrowserProvider(window.ethereum);
 
 const main = async () => {
   const balanceVal = await provider.getBalance(
@@ -8,6 +8,26 @@ const main = async () => {
   );
   const balanceVaWei = ethers.formatEther(balanceVal);
   console.log(balanceVaWei);
+  linkInfuraNode();
+};
+
+// 链接 Infura 节点
+const linkInfuraNode = async () => {
+  const INFURA_ID = "55c4d2d02019481f93e965bee6710e95";
+
+  // 连接以太坊主网
+  const providerETH = new ethers.JsonRpcProvider(
+    `https://mainnet.infura.io/v3/${INFURA_ID}`
+  );
+
+  console.log("providerETH =>", providerETH);
+
+  // 连接Goerli测试网
+  const providerGoerli = new ethers.JsonRpcProvider(
+    `https://goerli.infura.io/v3/${INFURA_ID}`
+  );
+
+  console.log("providerGoerli =>", providerGoerli);
 };
 
 main();
